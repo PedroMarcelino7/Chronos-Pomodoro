2e871aebf4960ca2ca3291812b92c3afc05411b2
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-07-20 22:13:24 -0300

    Criando componente footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Input } from './components/Input/Input';
 import { Cycles } from './components/Cycles/Cycles';
 import { Button } from './components/Button/Button';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer/Footer';
 
 export function App() {
   return (
@@ -50,6 +51,10 @@ export function App() {
             <Button
               icon={<PlayCircleIcon />}
             />
+          </div>
+
+          <div className="formRow">
+            <Footer />
           </div>
         </form>
       </Container>
