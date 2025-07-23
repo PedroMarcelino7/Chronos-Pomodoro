6f86ff52eabea1f6b494860c82e11e3139b5198f
PedroMarcelino7 <pedropmarcelino2004@gmail.com>
2025-07-22 22:31:31 -0300

    Criando função de trocar tema da pagina e armazenando no localstorage

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,36 +1,72 @@
 import styles from './Menu.module.css'
 
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
+import { useState, useEffect } from 'react'
+
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react'
+
+type Theme = 'dark' | 'light'
 
 export function Menu() {
+    const [theme, setTheme] = useState<Theme>(() => {
+        return localStorage.getItem('theme') as Theme || 'dark'
+    })
+
+    const nextThemeIcon = {
+        dark: <SunIcon />,
+        light: <MoonIcon />
+    }
+
+    function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        e.preventDefault()
+
+        setTheme((prevTheme) => {
+            const nextTheme = prevTheme === 'dark' ? 'light' : 'dark'
+            return nextTheme
+        })
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme)
+        localStorage.setItem('theme', theme)
+    }, [theme])
+
     return (
         <nav className={styles.menu}>
             <a
                 href=""
                 className={styles.menuLink}
+                aria-label='Home'
+                title='Home'
             >
                 <HouseIcon />
             </a>
-            
+
             <a
                 href=""
                 className={styles.menuLink}
+                aria-label='History'
+                title='History'
             >
                 <HistoryIcon />
             </a>
-            
+
             <a
                 href=""
                 className={styles.menuLink}
+                aria-label='Settings'
+                title='Settings'
             >
                 <SettingsIcon />
             </a>
-            
+
             <a
                 href=""
                 className={styles.menuLink}
+                aria-label='Theme'
+                title='Theme'
+                onClick={(e) => handleThemeChange(e)}
             >
-                <SunIcon />
+                {nextThemeIcon[theme]}
             </a>
         </nav>
     )
